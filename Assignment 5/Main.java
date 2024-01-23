eab5b362014e1461a4850c179432bcde9d4952d0
Shahin Alam <43540136+Shahin-Alam2@users.noreply.github.com>
2024-01-24 00:42:38 +0600

    Update Main.java

--- Assignment 5/Main.java
+++ Assignment 5/Main.java
@@ -1,1 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        Order myOrder = new Order();
+        myOrder.addToCart("Tesla");
+        myOrder.addToCart("iPhone 15 Pro Max");
+        myOrder.addToCart("Private Jet");
 
+        OrderFulfillmentProxy orderFulfillment = new OrderFulfillmentProxy();
+
+        WarehouseReal warehouse1 = new WarehouseReal();
+        WarehouseReal warehouse2 = new WarehouseReal();
+
+        warehouse1.addToInventory("Tesla", 1);
+        warehouse1.addToInventory("iPhone 15 Pro Max", 0);
+        warehouse1.addToInventory("Private Jet", 4);
+
+        warehouse2.addToInventory("Tesla", 2);
+        warehouse2.addToInventory("iPhone 15 Pro Max", 10);
+        warehouse2.addToInventory("Private Jet", 1);
+
+        orderFulfillment.addWarehouse(warehouse1);
+        orderFulfillment.addWarehouse(warehouse2);
+
+        orderFulfillment.completeOrder(myOrder);
+    }
+}
